afd7bbe71177c868d6b9bc8779b5674886222aba
AngelicaBR9 <angelica.burgos.ramos77999@gmail.com>
2023-11-30 18:39:00 -0400

    Updates

---
+++ script.js
@@ -0,0 +1,49 @@
+const btnmenuOpcion = document.getElementById(`btnclose`)
+const openMenu = () => {
+    const element = document.getElementById("sqlflotante");
+  element.classList.add("hidden");
+}
+
+const imgdespliegue = document.getElementById(`imagensql`)
+const openImgSQL = () => {
+    const element = document.getElementById("sqlflotante");
+  element.classList.remove("hidden");
+}
+
+
+
+const btnflothtml = document.getElementById(`btnhtml`)
+const openBTNhtml = () => {
+    const elementhtml = document.getElementById("htmlflotante");
+    elementhtml.classList.add("hidden");
+} 
+const imgdesplieguehtml = document.getElementById(`imagenhtml`)
+const openImgHTML = () => {
+    const elementhtml = document.getElementById("htmlflotante");
+    elementhtml.classList.remove("hidden");
+}
+
+
+const btnflotphp = document.getElementById(`btncphp`)
+const openphp = () => {
+    const elementphp = document.getElementById("phpflotante");
+    elementphp.classList.add("hidden");
+}
+const imgdesplieguephp = document.getElementById(`imagenphp`)
+const openImgPHP = () => {
+    const elementphp = document.getElementById("phpflotante");
+    elementphp.classList.remove("hidden");
+}
+
+
+
+const btnflotjava = document.getElementById(`btnJAVA`)
+const openjava = () => {
+    const elementjava = document.getElementById("javaflotante");
+    elementjava.classList.add("hidden");
+}
+const imgdesplieguejava = document.getElementById(`imagenjava`)
+const openImgJAVA = () => {
+    const elementjava = document.getElementById("javaflotante");
+    elementjava.classList.remove("hidden");
+}
