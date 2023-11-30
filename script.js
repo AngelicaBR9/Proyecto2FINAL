b7161c7372779e300145c6f5d40814b74a54d5e4
AngelicaBR9 <angelica.burgos.ramos77999@gmail.com>
2023-11-30 18:34:18 -0400

    adicion de evento

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
-// Función para ir a una sección
-function irASeccion(idSeccion) {
-    // Obtiene el elemento de la sección
-    const seccion = document.getElementById(idSeccion);
-  
-    // Centra la sección en la pantalla
-    seccion.scrollIntoView({ behavior: "smooth" });
-  }
-  
-  // Escucha el evento click en el botón de la sección 1
-  document.getElementById("btnAcerca").addEventListener("click", () => {
-    // Llama a la función para ir a la sección 1
-    irASeccion("seccion-acerca");
-  });
-  
-  // Escucha el evento click en el botón de la sección 2
-  document.getElementById("boton-seccion-2").addEventListener("click", () => {
-    // Llama a la función para ir a la sección 2
-    irASeccion("seccion-2");
-  });
+const btnmenuOpcion = document.getElementById(`btnclose`)
+const openMenu = () => {
+    const element = document.getElementById("sqlflotante");
+  element.classList.add("hidden");
+}
+
+const imgdespliegue = document.getElementById(`imagensql`)
+const openImgSQL = () => {
+    const element = document.getElementById("sqlflotante");
+  element.classList.remove("hidden");
+}
+
+
+
+const btnflothtml = document.getElementById(`btnhtml`)
+const openBTNhtml = () => {
+    const elementhtml = document.getElementById("htmlflotante");
+    elementhtml.classList.add("hidden");
+} 
+const imgdesplieguehtml = document.getElementById(`imagenhtml`)
+const openImgHTML = () => {
+    const elementhtml = document.getElementById("htmlflotante");
+    elementhtml.classList.remove("hidden");
+}
+
+
+const btnflotphp = document.getElementById(`btncphp`)
+const openphp = () => {
+    const elementphp = document.getElementById("phpflotante");
+    elementphp.classList.add("hidden");
+}
+const imgdesplieguephp = document.getElementById(`imagenphp`)
+const openImgPHP = () => {
+    const elementphp = document.getElementById("phpflotante");
+    elementphp.classList.remove("hidden");
+}
+
+
+
+const btnflotjava = document.getElementById(`btnJAVA`)
+const openjava = () => {
+    const elementjava = document.getElementById("javaflotante");
+    elementjava.classList.add("hidden");
+}
+const imgdesplieguejava = document.getElementById(`imagenjava`)
+const openImgJAVA = () => {
+    const elementjava = document.getElementById("javaflotante");
+    elementjava.classList.remove("hidden");
+}
